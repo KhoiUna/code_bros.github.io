42c08442ddc6d2a922808a480d3f16718b49f1d9
Khoi Nguyen <63745633+KhoiUna@users.noreply.github.com>
2020-05-02 12:29:16 -0500

    Update and rename myscript.js to js/script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let person = window.prompt("Hi there!\nWhat's your name?");
+let txt;
+
+if (person === null || person === "") {
+  txt = '"Welcome to Our Projects!"';
+} else {
+  txt = '"Hello ' + person + '! Welcome to Our Projects!"';
+}
+
+document.getElementById("hello").innerHTML = txt.toUpperCase();
+
+<script>
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip(); 
+});
+</script>
--- myscript.js
+++
@@ -1,2 +0,0 @@
-alert("Jun & Khoi haven't created this page! Please go HOME!")
-document.write("<h1>Go <a href='index.html'>Home</a></h1>")
